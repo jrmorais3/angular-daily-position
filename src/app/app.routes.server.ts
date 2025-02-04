25e3b83e7a6bc517c54e5bdfd21ff25f3e26b2dc
Junior Morais <jrmorais3@gmail.com>
2025-02-04 19:53:36 -0300

    adding new config

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,4 +1,13 @@
+import { Route, Routes } from '@angular/router';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'login'
+  }
+]
 
 export const serverRoutes: ServerRoute[] = [
   {
